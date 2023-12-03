f340eca387c177bb0a3f51a3120011b73512c4ec
7Ji <pugokushin@gmail.com>
2023-12-03 17:14:22 +0800

    main: add warning

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use pkgbuild::parse_multi;
 
 fn main() {
+    eprintln!("This executable is only for simple testing purposes and the \
+        output should never be used in actual production. Use the library \
+        directly instead.");
     let mut args = std::env::args_os();
     let _ = args.next();
     let pkgbuilds = parse_multi(args).unwrap();
