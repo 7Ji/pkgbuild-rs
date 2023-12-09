01c156fece5435595b40f3a501172be01e8916eb
7Ji <pugokushin@gmail.com>
2023-12-09 16:56:30 +0800

    examples/vercmp: stdout behavriou imitate pacman vercmp

--- examples/vercmp.rs
+++ examples/vercmp.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use pkgbuild::UnorderedVersion;
 
 fn main() {
@@ -6,6 +8,12 @@ fn main() {
     let arg_ver2 = args.nth(0).unwrap();
     let ver1 = UnorderedVersion::try_from(arg_ver1.as_str()).unwrap();
     let ver2 = UnorderedVersion::try_from(arg_ver2.as_str()).unwrap();
-    println!("Comparing version '{}' as '{:?}' and version '{}' as '{:?}': {:?}", 
-        arg_ver1, ver1, arg_ver2, ver2, ver1.cmp(&ver2));
+    let order = ver1.cmp(&ver2);
+    eprintln!("Comparing version '{}' as '{:?}' and version '{}' as '{:?}': {:?}", 
+        arg_ver1, ver1, arg_ver2, ver2, order);
+    match order {
+        Ordering::Greater => println!("1"),
+        Ordering::Equal => println!("0"),
+        Ordering::Less => println!("-1")
+    }
 }
