afd7dc11468fa51c54610fb8f98ce2233f524c19
7Ji <pugokushin@gmail.com>
2023-12-18 14:51:26 +0800

    script: better handling of in-function generated variable
    fixes the breakage when parsing arch offiical rhvoice pkg

--- src/script/22_func_extract_package_vars.bash
+++ src/script/22_func_extract_package_vars.bash
@@ -3,9 +3,21 @@ extract_package_vars() { #1 suffix
   IFS=$'\n'
   local lines=($(declare -f package"$1"))
   IFS="${ifs_stored}"
+  local buffer=
   for line in "${lines[@]:2:$((${#lines[@]}-3))}"; do 
-    if [[ "${line}" =~ (depends|provides)'=('* ]]; then
-      eval "${line}"
+    if [[ "${buffer}" ]]; then
+      buffer+="
+      ${line}"
+      if [[ "${line}" == *');' ]]; then
+        eval "${buffer}"
+        buffer=
+      fi
+    elif [[ "${line}" =~ (depends|provides)'=('* ]]; then
+      if [[ "${line}" == *');' ]]; then
+        eval "${line}"
+      else
+        buffer="${line}"
+      fi
     fi
   done
 }
