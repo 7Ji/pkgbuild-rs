6a3af5c586c2f8022234bcf11aee727f9f68dd87
7Ji <pugokushin@gmail.com>
2024-03-05 17:51:22 +0800

    temp commit for improving .srcinfo()

--- examples/printsrcinfo.rs
+++ examples/printsrcinfo.rs
@@ -43,8 +43,16 @@ where
 }
 
 fn main() {
+    env_logger::Builder::from_env(
+        env_logger::Env::default().filter_or(
+            "ARB_LOG_LEVEL", "info")
+        ).target(env_logger::Target::Stdout).init();
     let path = std::env::args_os().nth(1);
-    let pkgbuild = pkgbuild::parse_one(path).unwrap();
+    let script = ParserScriptBuilder::new().build(Some("/tmp/parser.sh")).unwrap();
+    let options = ParserOptions::default();
+    let parser = Parser { script, options };
+    let pkgbuild = parser.parse_one(path).unwrap();
+    // let pkgbuild = pkgbuild::parse_one(path).unwrap();
     print!("{}", pkgbuild.srcinfo());
     println!("{}", &pkgbuild);
 }
