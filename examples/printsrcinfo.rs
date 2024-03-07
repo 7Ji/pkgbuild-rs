81ca5ba001e0b77075ec9214e61f86cca6fe8266
7Ji <pugokushin@gmail.com>
2024-03-07 15:26:02 +0800

    examples/printsrcinfo: cleaner call chain

--- examples/printsrcinfo.rs
+++ examples/printsrcinfo.rs
@@ -1,13 +1,3 @@
 fn main() {
-    // env_logger::Builder::from_env(
-    //     env_logger::Env::default().filter_or(
-    //         "ARB_LOG_LEVEL", "info")
-    //     ).target(env_logger::Target::Stdout).init();
-    let path = std::env::args_os().nth(1);
-    let script = pkgbuild::ParserScriptBuilder::new().build(Some("/tmp/parser.sh")).unwrap();
-    let options = pkgbuild::ParserOptions::default();
-    let parser = pkgbuild::Parser { script, options };
-    let pkgbuild = parser.parse_one(path).unwrap();
-    // let pkgbuild = pkgbuild::parse_one(path).unwrap();
-    print!("{}", pkgbuild.srcinfo());
+    print!("{}", pkgbuild::parse_one(std::env::args_os().nth(1)).unwrap().srcinfo());
 }
