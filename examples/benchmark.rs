6aac7798ea7d3538c121965b48508d30f1cf3b22
7Ji <pugokushin@gmail.com>
2024-03-08 11:24:43 +0800

    examples/benchmark: switch mt and st

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -8,12 +8,8 @@ fn main() {
         list.push(entry.file_name())
     }
     list.sort_unstable();
+    let list_backup = list.clone();
     println!("Count of PKGBUILDs: {}", list.len());
-    println!("Testing single-thread reading...");
-    let mut time_start = Instant::now();
-    let mut pkgbuilds = pkgbuild::parse_multi(&list).unwrap();
-    println!("Single-thread reading took {} seconds", (Instant::now() - time_start).as_secs_f64());
-    println!("First PKGBUILD is {}, last is {}", pkgbuilds.first().unwrap().pkgbase, pkgbuilds.last().unwrap().pkgbase);
     let mut step: usize = list.len() / std::thread::available_parallelism().unwrap();
     let mut lists: Vec<Vec<OsString>> = Vec::new();
     while ! list.is_empty() {
@@ -24,16 +20,21 @@ fn main() {
         lists.push(list.drain((len - step)..len).collect())
     }
     println!("Testing {}-thread reading...", lists.len());
-    time_start = Instant::now();
+    let mut time_start = Instant::now();
     let threads: Vec<JoinHandle<Vec<Pkgbuild>>> = 
         lists.into_iter().map(|list|spawn(
             move ||pkgbuild::parse_multi(list).unwrap())).collect();
     let mut chunks: Vec<Vec<Pkgbuild>> = threads.into_iter().map(
         |thread|thread.join().unwrap()).collect();
-    pkgbuilds.clear();
+    let mut pkgbuilds = Vec::new();
     while let Some(mut chunk) = chunks.pop() {
         pkgbuilds.append(&mut chunk);
     }
     println!("Multi-thread reading took {} seconds", (Instant::now() - time_start).as_secs_f64());
+    println!("First PKGBUILD is {}, last is {}", pkgbuilds.first().unwrap().pkgbase, pkgbuilds.last().unwrap().pkgbase);
+    println!("Testing single-thread reading...");
+    time_start = Instant::now();
+    pkgbuilds = pkgbuild::parse_multi(&list_backup).unwrap();
+    println!("Single-thread reading took {} seconds", (Instant::now() - time_start).as_secs_f64());
     println!("First PKGBUILD is {}, last is {}", pkgbuilds.first().unwrap().pkgbase, pkgbuilds.last().unwrap().pkgbase);
 }
